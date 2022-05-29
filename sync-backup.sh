5f1a45203ff74ca76ebf6585a1683a3ba7008f5c
Chuck Boecking <chuck@chuboe.com>
2022-05-29 11:24:27 -0500

    added date updated

--- sync-backup.sh
+++ sync-backup.sh
@@ -64,6 +64,7 @@ declare -A BU_PRIVATE
 #BU_PRIVATE[postgres-config]="/etc/postgresql/12/main/"
 #BU_PRIVATE[postgres-history]=~/.psql_history.d/
 #BU_PRIVATE[sync-backup.sh]=~/chuboe-system-backup/sync-backup.sh
+#BU_PRIVATE[sync-lastupdate.txt]=~/chuboe-system-backup/sync-lastupdate.txt
 
 
 #### Public Backup Array ####
